93894f175dea84a506040a44010494627b88c964
yuexie.zhou <yuexie.zhou@thoughtworks.com>
2020-03-20 09:46:15 +0800

    [build]: remove app service and app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MomentsController } from './moments/moments.controller';
-import { MomentsService } from './moments/moments.service';
 import { MomentsModule } from './moments/moments.module';
 
 @Module({
   imports: [MomentsModule],
-  controllers: [AppController, MomentsController],
-  providers: [AppService, MomentsService],
 })
 export class AppModule {}
--- src/moments/moments.module.ts
+++ src/moments/moments.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MomentsController } from './moments.controller';
+import { MomentsService } from './moments.service';
 
-@Module({})
+@Module({
+  controllers: [MomentsController],
+  providers: [MomentsService],
+})
 export class MomentsModule {}
