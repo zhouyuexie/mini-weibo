9dfd0bfbe5e0a8a380062ec8a89124f03882bde6
yuexie.zhou <yuexie.zhou@thoughtworks.com>
2020-03-20 09:27:04 +0800

    [feat]: add moments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MomentsController } from './moments/moments.controller';
+import { MomentsService } from './moments/moments.service';
+import { MomentsModule } from './moments/moments.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MomentsModule],
+  controllers: [AppController, MomentsController],
+  providers: [AppService, MomentsService],
 })
 export class AppModule {}
---
+++ src/moments/moments.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('moments')
+export class MomentsController {}
